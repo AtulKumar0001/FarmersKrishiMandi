9ec024784ea0fdc7ce154445d1b17be81c890afd
Atul Kumar <95268815+AtulKumar0001@users.noreply.github.com>
2024-10-24 16:48:20 +0530

    fixed the build errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['sbzxabupqcuwinesuoim.supabase.co'], // Add your Supabase domain here
-    },
-    i18n: {
-      locales: ['en', 'hi', 'bn', 'te', 'mr', 'ta', 'gu', 'kn', 'ml', 'pa', 'ur'],
-      defaultLocale: 'en',
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    domains: ['sbzxabupqcuwinesuoim.supabase.co'],
+  },
+  i18n: {
+    locales: ['en', 'hi', 'bn', 'te', 'mr', 'ta', 'gu', 'kn', 'ml', 'pa', 'ur'],
+    defaultLocale: 'en',
+  },
+};
+
+export default nextConfig;
