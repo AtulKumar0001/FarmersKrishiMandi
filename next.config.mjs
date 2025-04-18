5fd769908b522df7127aa3b3944eed098a2c2c98
atulkumar <atul.kumar5@bigbasket.com>
2025-04-18 20:09:19 +0530

    added domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ['sbzxabupqcuwinesuoim.supabase.co'], // Add your Supabase domain here
+      domains: [process.env.NEXT_SUPABASE_DOMAIN], // Add your Supabase domain here
     },
     i18n: {
       locales: ['en', 'hi', 'bn', 'te', 'mr', 'ta', 'gu', 'kn', 'ml', 'pa', 'ur'],
